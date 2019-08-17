a78a18f363d06afbe430312b19c3a4eee8c32a3a
Edina Orban <Amarillia@users.noreply.github.com>
2019-08-17 19:37:34 +0100

    added style and drum pad layout

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <h1>Drum Machine</h1>
+    <div id="drum-machine">
+      <div id="display">
+        <AppDrumPad msg="Q"></AppDrumPad>
+        <AppDrumPad msg="W"></AppDrumPad>
+        <AppDrumPad msg="E"></AppDrumPad>
+        <AppDrumPad msg="A"></AppDrumPad>
+        <AppDrumPad msg="S"></AppDrumPad>
+        <AppDrumPad msg="D"></AppDrumPad>
+        <AppDrumPad msg="Z"></AppDrumPad>
+        <AppDrumPad msg="X"></AppDrumPad>
+        <AppDrumPad msg="C"></AppDrumPad>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppDrumPad from "./components/AppDrumPad.vue";
+
+export default {
+  name: "app",
+  components: {
+    AppDrumPad
+  },
+
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Bubbler+One&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  background-color: #a6592d;
+}
+
+#app {
+  font-family: "Bubbler One", sans-serif;
+  font-weight: 100;
+  text-align: center;
+}
+#drum-machine {
+  width: 80%;
+  margin: 0 auto;
+}
+
+#display {
+  display: grid;
+  grid-template-columns: 125px 125px 125px;
+  grid-template-rows: 125px 125px 125px;
+  grid-column-gap: 15px;
+  grid-row-gap: 15px;
+  justify-content: center;
+  background-color: #f2913d;
+  border-radius: 25px;
+  padding: 15px;
+}
+
+h1 {
+  font-size: 50px;
+  margin: 25px;
+  color: #fff;
+}
+</style>
---
+++ src/components/AppDrumPad.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="drum-pad">
+    <button>{{msg}}
+      <audio src="" class="clip"></audio>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppDrumPad",
+  props: {
+    msg: String,
+   }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.drum-pad {
+background:none;
+}
+
+button {
+  border: 1px solid #f2c84b;
+  background-color: #f2c84b;
+  font-size: 55px;
+  width: 125px;
+  height: 125px;
+  cursor: pointer;
+  border-radius: 25px;
+}
+</style>
